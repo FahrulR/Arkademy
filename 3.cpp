1a84fe4486add7798f6bafb151ae0b5ac63c4e8a
FahrulR <44598352+FahrulR@users.noreply.github.com>
2019-06-29 13:54:10 +0700

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,32 @@
+#include <iostream> 
+using namespace std; 
+void printPattern(int); 
+int main() 
+{ 
+    int n = 6; 
+  
+    printPattern(n); 
+} 
+void printPattern(int n) 
+{ 
+    int i, j; 
+    for (i = 1; i <= n; i++) { 
+        // Print  spaces 
+        for (j = 1; j < i; j++) { 
+            cout << " "; 
+        } 
+  
+        // Print hollow inverted pyramid 
+        for (j = 1; j <= (n * 2 - (2 * i - 1)); j++) { 
+  
+            if (i == 1 || j == 1 || j == (n * 2 - (2 * i - 1))) { 
+                cout << "*"; 
+            } else { 
+                cout << " "; 
+            } 
+        } 
+  
+        // next line 
+        cout << endl; 
+    } 
+} 
